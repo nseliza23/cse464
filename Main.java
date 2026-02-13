4af6795f84c4f9a32069c244ced4e76a9bd4aa22
nseliza23 <nshibuel@asu.edu>
2026-02-12 19:46:27 -0700

    First commit of Main.java for Lab Assignment

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[]args) {
+		String place1 = "Arizona";
+		String place2 = "California";
+		String month = "May";
+		System.out.println("We are going on a roadtrip from " + place1 + " to " + place2 + " in " + month + "!");
+	}
+}
